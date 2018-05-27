825a5a299a9b9d1145792bf1f986e8052d633c00
likesatie <likesatie@gmail.com>
2018-05-27 07:39:35 +0300

    commit js

--- js/script.js
+++ js/script.js
@@ -5,16 +5,42 @@ $(document).ready(function () {
     $('.navbar-toggler').toggleClass('change')
 
   })
+
   $(window).scroll(function () {
 
     let position = $(this).scrollTop();
 
     if (position >= 718) {
       $('.navbar').addClass('navbar-background');
-      $()
+      $('.navbar').addClass('fixed-top');
+    } else {
+      $('.navbar').removeClass('navbar-background');
+      $('.navbar').removeClass('fixed-top');
     }
+  })
+
+  $('.nav-item a, .header-link, #back-to-top').click(function (link) {
+    link.preventDefault();
+
+    let target = $(this).attr('href');
+
+    $('html, body').stop().animate({
+      scrollTop: $(target).offset().top - 25
+    }, 2000);
 
   })
+
+  $(window).scroll(function () {
+
+    let position = $(this).scrollTop();
+
+    if (position >= 718) {
+      $('#back-to-top').addClass('scrollTop');
+    } else {
+      $('#back-to-top').removeClass('scrollTop');
+    }
+  })
+
   $('.parent-container').magnificPopup({
     delegate: 'a', // child items selector, by clicking on it popup will open
     type: 'image',
