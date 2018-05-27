2fbfcd3d2e869d09b5e236ced2fc7beae2f51310
likesatie <likesatie@gmail.com>
2018-05-27 07:14:17 +0300

    commit 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function () {
+
+  $('.navbar-toggler').click(function () {
+
+    $('.navbar-toggler').toggleClass('change')
+
+  })
+  $(window).scroll(function () {
+
+    let position = $(this).scrollTop();
+
+    if (position >= 718) {
+      $('.navbar').addClass('navbar-background');
+      $()
+    }
+
+  })
   $('.parent-container').magnificPopup({
     delegate: 'a', // child items selector, by clicking on it popup will open
     type: 'image',
